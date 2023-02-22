4ee79df6a8cc5a394520b1fb1683c236ab637648
Chris Li <chris.lty07@gmail.com>
2023-02-22 04:05:54 -0500

    Build Restaurant Card and Dishes

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -12,12 +12,11 @@ const FeaturedRow = ({ id, title, description }) => {
         sanityClient
             .fetch(`*[_type == "featured" && _id == $id ] {..., restaurants[]->{...,dishes[]->,type-> {name}}}[0]`, { id })
             .then(data => {
+                console.log(data)
                 setRestaurants(data?.restaurants)
             })
             .catch(console.error)
-    }, [])
-
-    // console.log(data?.restaurants)
+    }, [id])
 
     return (
         <View>
@@ -51,50 +50,13 @@ const FeaturedRow = ({ id, title, description }) => {
                             genre={restaurant.type?.name}
                             address={restaurant.address}
                             short_description={restaurant.short_description}
-                            // dishes={[]}
+                            dishes={restaurant.dishes}
                             long={restaurant.long}
                             lat={restaurant.lat}
                         />
                     )
                 })}
-                {/* 
-                <RestaurantCard
-                    id="1"
-                    imgUrl="https://images-cdn.welcomesoftware.com/Zz1iYmRkMDJjNmI3NjIxMWViODgwY2E1MDAxNTJkMjNlNg==/Zz1hZTNkM2M1YTliOTcxMWViYWUxOWIxMDE0ZmI1ZjNlMA%3D%3D.jpg?width=1366"
-                    title="Yo! Sushi"
-                    rating="4.5"
-                    genre="Japanese"
-                    address="123 Main St"
-                    short_description="This is a Test Description"
-                    dishes={[]}
-                    long={20}
-                    lat={0}
-                />
-                <RestaurantCard
-                    id="1"
-                    imgUrl="https://images-cdn.welcomesoftware.com/Zz1iYmRkMDJjNmI3NjIxMWViODgwY2E1MDAxNTJkMjNlNg==/Zz1hZTNkM2M1YTliOTcxMWViYWUxOWIxMDE0ZmI1ZjNlMA%3D%3D.jpg?width=1366"
-                    title="Yo! Sushi"
-                    rating="4.5"
-                    genre="Japanese"
-                    address="123 Main St"
-                    short_description="This is a Test Description"
-                    dishes={[]}
-                    long={20}
-                    lat={0}
-                />
-                <RestaurantCard
-                    id="1"
-                    imgUrl="https://images-cdn.welcomesoftware.com/Zz1iYmRkMDJjNmI3NjIxMWViODgwY2E1MDAxNTJkMjNlNg==/Zz1hZTNkM2M1YTliOTcxMWViYWUxOWIxMDE0ZmI1ZjNlMA%3D%3D.jpg?width=1366"
-                    title="Yo! Sushi"
-                    rating="4.5"
-                    genre="Japanese"
-                    address="123 Main St"
-                    short_description="This is a Test Description"
-                    dishes={[]}
-                    long={20}
-                    lat={0}
-                /> 
-                 */}
+
             </ScrollView>
         </View>
     )
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { StarIcon } from 'react-native-heroicons/outline';
@@ -16,8 +17,25 @@ const RestaurantCard = ({
     long,
     lat,
 }) => {
+    const navigation = useNavigation();
+
     return (
-        <TouchableOpacity className="bg-white mr-3 shadow">
+        <TouchableOpacity
+            onPress={() => {
+                navigation.navigate('Restaurant', {
+                    id,
+                    imgUrl,
+                    title,
+                    rating,
+                    genre,
+                    address,
+                    short_description,
+                    dishes,
+                    long,
+                    lat,
+                })
+            }}
+            className="bg-white mr-3 shadow">
             <View>
                 <Image
                     source={{
